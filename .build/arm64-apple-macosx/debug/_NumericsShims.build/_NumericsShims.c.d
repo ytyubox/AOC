a0780b07fe51bc608e328c38328ce7290fc3b865
Tsungyu Yu <ytyubox@gmail.com>
2023-12-02 22:54:58 +0800

    2023

--- .build/arm64-apple-macosx/debug/_NumericsShims.build/_NumericsShims.c.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/tsungyu/code/AOC/.build/checkouts/swift-numerics/Sources/_NumericsShims/_NumericsShims.c \
-  /Users/tsungyu/code/AOC/.build/checkouts/swift-numerics/Sources/_NumericsShims/include/module.modulemap \
-  /Users/tsungyu/code/AOC/.build/checkouts/swift-numerics/Sources/_NumericsShims/include/_NumericsShims.h
